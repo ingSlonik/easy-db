5c50d4207b34f5402186e3be865d34c92e200f18
Filip Paulů <ing.fenix@seznam.cz>
2020-08-18 20:35:57 +0200

    Add file handling

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,21 +1,48 @@
-import * as fs from "fs";
-import { resolve } from "path";
+import { resolve, basename } from "path";
+import { promises, existsSync } from "fs";
+const { readFile, mkdir, writeFile, unlink } = promises;
 
-import easyDB, { Data } from "easy-db-core";
+import easyDB, { getRandomId, Data } from "easy-db-core";
 
-const folderDB = "easy-db";
+
+type Partial<T> = {
+    [P in keyof T]?: T[P];
+};
+
+type Configuration = {
+    fileFolder: string,
+    fileUrl: string,
+    folderDB: string,
+};
+
+// One configuration for whole runtime
+const configuration: Configuration = {
+    fileFolder: "easy-db-files",
+    fileUrl: "/easy-db-files",
+    folderDB: "easy-db",
+}
+
+export function configure(config: Partial<Configuration>) {
+    for (const key in config) {
+        configuration[key] = config[key];
+    }
+}
 
 export const { insert, select, update, remove } = easyDB({
     async saveCollection(name: string, data: Data) {
-        if (!fs.existsSync(folderDB)) {
-            fs.mkdirSync(folderDB);
+        if (!existsSync(configuration.folderDB)) {
+            await mkdir(configuration.folderDB);
         }
-        fs.writeFileSync(resolve(folderDB, `${name}.json`), JSON.stringify(data, null, "    "), "utf8");
+        await writeFile(
+            resolve(configuration.folderDB, `${name}.json`),
+            JSON.stringify(data, null, "    "), "utf8"
+        );
+        return;
     },
     async loadCollection(name: string): Promise<null | Data> {
-        const file = resolve(folderDB, `${name}.json`);
-        if (fs.existsSync(file)) {
-            const content = fs.readFileSync(file, "utf8");
+        const file = resolve(configuration.folderDB, `${name}.json`);
+        if (existsSync(file)) {
+            const content = await readFile(file, "utf8");
             try {
                 const data = JSON.parse(content);
                 if (data !== null && typeof data === "object") {
@@ -31,4 +58,40 @@ export const { insert, select, update, remove } = easyDB({
             return null;
         }
     },
+    async saveFile(base64: string) {
+        if (!existsSync(configuration.fileFolder)) {
+            await mkdir(configuration.fileFolder);
+        }
+
+        let extension = "bin";
+        if (base64.startsWith("data:")) {
+            const indexFrom = base64.indexOf('/');
+            if (indexFrom > -1) {
+                const indexTo = base64.indexOf(';base64');
+                if (indexFrom < indexTo) {
+                    extension = base64.substring(indexFrom + 1, indexTo);
+                }
+            }
+        }
+
+        const fileName = getFreeFileName(configuration.fileFolder, extension);
+        await writeFile(
+            resolve(configuration.fileFolder, fileName),
+            Buffer.from(base64, "base64"),
+        );
+
+        return fileName;
+    },
+    async removeFile(path: string) {
+        await unlink(resolve(configuration.fileFolder, basename(path)));
+    }
 });
+
+function getFreeFileName(path: string, extension: string): string {
+    const fileName = `${getRandomId()}.${extension}`;
+    if (existsSync(resolve(path, fileName))) {
+        return getFreeFileName(path, extension);
+    } else {
+        return fileName;
+    }
+}

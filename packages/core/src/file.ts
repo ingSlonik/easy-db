c2f3e68ef2426195b8422ea6d4203692c58a06c8
Filip Paulů <ing.fenix@seznam.cz>
2020-09-23 15:09:21 +0200

    Fix multiple file save and add comments

--- packages/core/src/file.ts
+++ packages/core/src/file.ts
@@ -41,11 +41,6 @@ function isFile(data: Data): boolean {
     }
 }
 
-function isNewFile(file: File): boolean {
-    // back compatibility
-    return typeof file.id !== "string" && isBase64(file.url);
-}
-
 export function getFile(url: string): File { 
     return {
         id: null,
@@ -64,33 +59,41 @@ export async function replaceFileData(
     update: Update,
 ): Promise<Data> {
     if (isFile(data)) {
-        if (isNewFile(data)) {
-            const url = await replaceFile(data.url);
-            const use = [ { collection, rowId } ];
-            const id = await insert(FILE_COLLECTION, (id: string): FileRow => ({ id, url, use }));
-            const file: File = { id, type: "EASY_DB_FILE", url };
-            return file;
-        } else {
-            if (typeof data.id === "string") {
-                const fileRow: null | FileRow = await select(FILE_COLLECTION, data.id);
-                if (fileRow) {
-                    await update(FILE_COLLECTION, data.id, {
-                        ...fileRow,
-                        use: [
-                            fileRow.use.filter(use => use.collection !== collection && use.rowId !== rowId),
-                            { collection, rowId }
-                        ],
-                    });
-                } else {
-                    await update(FILE_COLLECTION, data.id, {
-                        id: data.id,
-                        url: data.url,
-                        use: [ { collection, rowId } ],
-                    });
-                }
+        if (typeof data.id === "string") {
+            // file is parsed
+            const fileRow: null | FileRow = await select(FILE_COLLECTION, data.id);
+            if (fileRow) {
+                // update FileRow for situation when is one file in more collections/rows
+                await update(FILE_COLLECTION, data.id, {
+                    ...fileRow,
+                    use: [
+                        fileRow.use.filter(use => use.collection !== collection && use.rowId !== rowId),
+                        { collection, rowId }
+                    ],
+                });
+            } else {
+                // for situation that FileRow was lost
+                await update(FILE_COLLECTION, data.id, {
+                    id: data.id,
+                    url: data.url,
+                    use: [ { collection, rowId } ],
+                });
             }
 
             return data;
+        } else {
+            // file is not parsed
+            if (isBase64(data.url)) {
+                // new file to save to server
+                const url = await replaceFile(data.url);
+                const use = [ { collection, rowId } ];
+                const id = await insert(FILE_COLLECTION, (id: string): FileRow => ({ id, url, use }));
+                const file: File = { id, type: "EASY_DB_FILE", url };
+                return file;
+            } else {
+                // the url is defined by user
+                return data;
+            }
         }
     } else if (Array.isArray(data)) {
         const newData = [];
@@ -109,6 +112,8 @@ export async function replaceFileData(
     }
 }
 
+// this function has to be called after replaceFileData
+// that means that here is not new file to save
 export async function removeUpdatedFiles(
     newData: Data,
     oldData: Data,
@@ -128,9 +133,12 @@ export async function removeUpdatedFiles(
     for (const oldFile of oldFiles) {
         const { id, url } = oldFile;
         if (newFilesIds.indexOf(id) < 0) {
+            // these files was in ond row and is not in new row
             if (typeof id === "string" && !isBase64(url)) {
+                // when was saved on this server
                 const fileRow: null | FileRow = await select(FILE_COLLECTION, id);
                 if (fileRow) {
+                    // remove this collection/worId from FileRow
                     const use = fileRow.use.filter(use => use.collection !== collection && use.rowId !== rowId);
 
                     if (use.length === 0) {
@@ -138,10 +146,11 @@ export async function removeUpdatedFiles(
                         await remove(FILE_COLLECTION, id);
                         removeFile(url);
                     } else {
+                        // if file is used in other collection/worId keep file
                         await update(FILE_COLLECTION, id, { ...fileRow, use });
                     }
                 } else {
-                    // back compatibility
+                    // remove file without FileRow
                     removeFile(url);
                 }
             }
@@ -168,5 +177,6 @@ function getFilesFromData(data: Data, files: File[] = []): File[] {
 // Source: https://github.com/miguelmota/is-base64/blob/master/is-base64.js
 const regexIsBase64 = new RegExp("^(data:\\w+\\/[a-zA-Z\\+\\-\\.]+;base64,)(?:[A-Za-z0-9+\\/]{4})*(?:[A-Za-z0-9+\\/]{2}==|[A-Za-z0-9+\/]{3}=)?$", "gi");
 function isBase64(base64: string): boolean {
+    regexIsBase64.lastIndex = 0;
     return regexIsBase64.test(base64);
 }

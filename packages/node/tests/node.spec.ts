d4dfdb4ecd126569f4809ee1461934f762b502ce
Filip Paulů <ing.fenix@seznam.cz>
2020-09-23 15:10:13 +0200

    Add tests with save files

--- packages/node/tests/node.spec.ts
+++ packages/node/tests/node.spec.ts
@@ -1,10 +1,13 @@
+import { readFileSync, existsSync } from "fs";
+import { resolve } from "path";
+
 import { assert } from "chai";
 
 import { insert, select, update, remove, configure, file } from "../src/index";
 
 const DUMMY_FILE = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=";
 
-configure({ fileFolder: "./files", fileUrl: "/files" });
+configure({ fileFolder: "./files", fileUrl: "./files" });
 
 describe('Easy DB', () => {
     it('db API', () => {
@@ -53,6 +56,42 @@ describe('Easy DB', () => {
         const data = await select("test", id);
         assert.isString(data.photo.url);
         assert.notEqual(data.photo.url, DUMMY_FILE);
+        await remove("test", id);
+    });
+
+    it('add two files in one row', async () => {
+        const id = await insert("test", {
+            name: "Example User",
+            picture: file(DUMMY_FILE),
+            photo: file(DUMMY_FILE),
+        });
+        const data = await select("test", id);
+        assert.isString(data.photo.url);
+        assert.notEqual(data.photo.url, DUMMY_FILE, "First file is not converted.");
+        assert.isString(data.picture.url);
+        assert.notEqual(data.picture.url, DUMMY_FILE, "Second file is not converted.");
+        await remove("test", id);
+    });
+
+    it('is file saved', async () => {
+        const id = await insert("test", {
+            name: "Example User",
+            photo: file(DUMMY_FILE),
+        });
+        const data = await select("test", id);
+        const filePath = resolve(data.photo.url)
+        assert.isTrue(existsSync(filePath), "File is not it not in file system.");
+        await remove("test", id);
+    });
+
+    it('keep file content', async () => {
+        const id = await insert("test", {
+            name: "Example User",
+            photo: file(DUMMY_FILE),
+        });
+        const data = await select("test", id);
+        const fileBase64 = readFileSync(resolve(data.photo.url), "base64");
+        assert.equal(fileBase64, DUMMY_FILE.substr(22));
         await remove("test", id);
     });
 

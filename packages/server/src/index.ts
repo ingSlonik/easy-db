3c11d5bee640140f854a6bb8fecadf47e6c1d93f
Filip Paulů <ing.fenix@seznam.cz>
2020-10-25 22:35:14 +0100

    Add cors to easy-db-server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 import { select, insert, update, remove } from "easy-db-node";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/:collection", async (req, res) => {

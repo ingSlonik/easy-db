d976bd8ed1dd0762edbb34179b9e070beaea5c53
Filip Paulů <ing.fenix@seznam.cz>
2021-03-18 20:51:54 +0100

    Update react-native package

--- packages/react-native/src/react-native.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "react-native" {
-    type AsyncStorageType = {
-        setItem(key: string, value: string): Promise<void>,
-        getItem(key: string): Promise<string>,
-    }
-    
-    export const AsyncStorage: AsyncStorageType;
-}

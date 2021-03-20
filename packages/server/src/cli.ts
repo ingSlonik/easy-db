b33a744764c90e033429ec4e007806b4ad4f1a60
Filip Paulů <ing.fenix@seznam.cz>
2021-03-20 12:22:09 +0100

    Implement new server API

--- packages/server/src/cli.ts
+++ packages/server/src/cli.ts
@@ -2,22 +2,34 @@
 import { resolve } from "path";
 import yargs from "yargs";
 
-import app from "./";
+import { express, useCors, useToken, useEasyDB } from "./";
 
 const options = yargs
     .usage("Usage: easy-db-server --port <port> -html ./index.html")
     .option("p", { alias: "port", describe: "PORT", type: "number", demandOption: false })
     .option("h", { alias: "html", describe: "Index HTML file", type: "string", demandOption: false })
+    .option("t", { alias: "easy-db-token", describe: "Security token", type: "string", demandOption: false })
     .argv;
 
 const PORT = process.env.PORT || options.port || 80;
 const INDEX = options.index || resolve(__dirname, "..", "index.html");
+const TOKEN = process.env.EASY_DB_TOKEN || options.t || null;
+
+const app = express();
 
 if (typeof INDEX === "string") {
     app.get("/", function (req, res) {
         res.sendFile(INDEX);
     });
 }
+
+useCors(app);
+
+if (typeof TOKEN === "string") {
+    useToken(app, TOKEN);
+}
+
+useEasyDB(app);
 
 const server = app.listen(PORT, () => console.log(`Easy DB server is running at http://localhost:${PORT}.`));
 
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,99 +1,113 @@
-import express from "express";
+import express, { Express } from "express";
 import cors from "cors";
 
 import { Query } from 'mingo';
 import { select, insert, update, remove } from "easy-db-node";
 
-const app = express();
+export { default as express } from "express";
 
-app.use(cors());
-app.use(express.json());
+export function useCors(app: Express) {
+    app.use(cors());
+}
 
-app.get("/api/:collection", async (req, res) => {
-    const { collection } = req.params;
-    const data = await select(collection);
+export function useToken(app: Express, token: string) {
+    app.use((req, res, next) => {
+        const tokenFromHeader = req.headers["easy-db-token"];
 
-    if (typeof req.query["query"] === "string") {
-        let query = {};
-        try {
-            query = JSON.parse(req.query["query"]);
-        } catch (e) {
-            res.status(400);
-            res.send(e.message);
-            return;
-        }
-
-        const dataForQuery = Object.keys(data).map(_id => ({ ...data[_id], _id }));
-
-        const filteredData = new Query(query).find(dataForQuery).all();
-
-        if (req.query["easy-db-client"] === "true") {
-            const easyDbData = {};
-            filteredData.forEach(({ _id, ...row }) => easyDbData[_id] = row);
-
-            res.type("json");
-            res.send(easyDbData);
+        if (typeof tokenFromHeader === "string" && tokenFromHeader === token) {
+            next();
         } else {
-            res.type("json");
-            res.send(filteredData);
+            res.status(401);
+            res.send("Not authorized request.");
         }
-    } else {
-        if (req.query["easy-db-client"] === "true") {
-            res.type("json");
-            res.send(data);
+    });
+}
+
+export function useEasyDB(app: Express) {
+
+    app.use(express.json());
+
+    app.get("/api/:collection", async (req, res) => {
+        const { collection } = req.params;
+        const data = await select(collection);
+    
+        if (typeof req.query["query"] === "string") {
+            let query = {};
+            try {
+                query = JSON.parse(req.query["query"]);
+            } catch (e) {
+                res.status(400);
+                res.send(e.message);
+                return;
+            }
+    
+            const dataForQuery = Object.keys(data).map(_id => ({ ...data[_id], _id }));
+    
+            const filteredData = new Query(query).find(dataForQuery).all();
+    
+            if (req.query["easy-db-client"] === "true") {
+                const easyDbData = {};
+                filteredData.forEach(({ _id, ...row }) => easyDbData[_id] = row);
+    
+                res.type("json");
+                res.send(easyDbData);
+            } else {
+                res.type("json");
+                res.send(filteredData);
+            }
         } else {
-            const rows = Object.keys(data).map(_id => ({ ...data[_id], _id }));
-
-            res.type("json");
-            res.send(rows);
+            if (req.query["easy-db-client"] === "true") {
+                res.type("json");
+                res.send(data);
+            } else {
+                const rows = Object.keys(data).map(_id => ({ ...data[_id], _id }));
+    
+                res.type("json");
+                res.send(rows);
+            }
         }
-    }
-});
-
-app.get("/api/:collection/:id", async (req, res) => {
-    const { collection, id } = req.params;
-    const row = await select(collection, id);
-
-    res.type("json");
-    res.send(row !== null ? row : JSON.stringify(null));
-});
-
-app.post("/api/:collection", async (req, res) => {
-    const { collection } = req.params;
-    const id = await insert(collection, req.body);
-
-    res.type("json");
-    res.send(JSON.stringify(id));
-});
-
-app.put("/api/:collection/:id", async (req, res) => {
-    const { collection, id } = req.params;
-    await update(collection, id, req.body);
-
-    res.type("json");
-    res.send(JSON.stringify(null));
-});
-
-app.patch("/api/:collection/:id", async (req, res) => {
-    const { collection, id } = req.params;
-    const row = await select(collection, id);
-
-    await update(collection, id, { ...row, ...req.body });
-    res.type("json");
-    res.send(JSON.stringify(null));
-});
-
-app.delete("/api/:collection/:id", async (req, res) => {
-    const { collection, id } = req.params;
-    await remove(collection, id);
-
-    res.type("json");
-    res.send(JSON.stringify(null));
-});
-
-app.use("/easy-db-files", express.static("easy-db-files"));
-
-export { app };
-
-// back compatibility
-export default app;
+    });
+    
+    app.get("/api/:collection/:id", async (req, res) => {
+        const { collection, id } = req.params;
+        const row = await select(collection, id);
+    
+        res.type("json");
+        res.send(row !== null ? row : JSON.stringify(null));
+    });
+    
+    app.post("/api/:collection", async (req, res) => {
+        const { collection } = req.params;
+        const id = await insert(collection, req.body);
+    
+        res.type("json");
+        res.send(JSON.stringify(id));
+    });
+    
+    app.put("/api/:collection/:id", async (req, res) => {
+        const { collection, id } = req.params;
+        await update(collection, id, req.body);
+    
+        res.type("json");
+        res.send(JSON.stringify(null));
+    });
+    
+    app.patch("/api/:collection/:id", async (req, res) => {
+        const { collection, id } = req.params;
+        const row = await select(collection, id);
+    
+        await update(collection, id, { ...row, ...req.body });
+        res.type("json");
+        res.send(JSON.stringify(null));
+    });
+    
+    app.delete("/api/:collection/:id", async (req, res) => {
+        const { collection, id } = req.params;
+        await remove(collection, id);
+    
+        res.type("json");
+        res.send(JSON.stringify(null));
+    });
+    
+    app.use("/easy-db-files", express.static("easy-db-files"));    
+}

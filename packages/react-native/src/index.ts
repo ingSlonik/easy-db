c93a9b245a7d22fa6fd648445d3c1fadbd4ccea3
Filip Paulů <ing.fenix@seznam.cz>
2021-09-05 23:43:35 +0200

    Update API for easy-db-react-native

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -4,24 +4,26 @@ import easyDB, { Data } from "easy-db-core";
 
 const prefix = "@easy-db:";
 
-export const { insert, select, update, remove } = easyDB({
-    async saveCollection(name: string, data: Data) {
-        // v2 await AsyncStorage.setItem(`@${prefix}${name}`, JSON.stringify(data));
-        await AsyncStorage.setItem(`${prefix}${name}`, JSON.stringify(data));
-    },
-    async loadCollection(name: string): Promise<null | Data> {
-        // v2 const content = await AsyncStorage.getItem(`@${prefix}${name}`);
-        const content = await AsyncStorage.getItem(`${prefix}${name}`);
-        try {
-            const data = JSON.parse(content);
-            if (data !== null && typeof data === "object") {
-                return data;
-            } else {
+export default function easyDBReactNative(conf?: {}) {
+    return easyDB({
+        async saveCollection(name: string, data: Data) {
+            // v2 await AsyncStorage.setItem(`@${prefix}${name}`, JSON.stringify(data));
+            await AsyncStorage.setItem(`${prefix}${name}`, JSON.stringify(data));
+        },
+        async loadCollection(name: string): Promise<null | Data> {
+            // v2 const content = await AsyncStorage.getItem(`@${prefix}${name}`);
+            const content = await AsyncStorage.getItem(`${prefix}${name}`);
+            try {
+                const data = JSON.parse(content);
+                if (data !== null && typeof data === "object") {
+                    return data;
+                } else {
+                    return null;
+                }
+            } catch (e) {
+                // TODO: write inconsistent data
                 return null;
             }
-        } catch (e) {
-            // TODO: write inconsistent data
-            return null;
-        }
-    },
-});
+        },
+    });
+}
--- packages/react-native/tests/react-native.spec.ts
+++ packages/react-native/tests/react-native.spec.ts
@@ -3,7 +3,8 @@ import "./asyncStorage";
 
 import { assert } from "chai";
 
-import { insert, select, update, remove } from "../src/index";
+import easyDB from "../src/index";
+const { insert, select, update, remove } = easyDB();
 
 describe('Easy DB', () => {
     it('db API', () => {

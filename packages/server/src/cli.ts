f2e863ba3bdcf0a31832d6c440233c4d5031dbec
Filip Paulů <ing.fenix@seznam.cz>
2021-10-20 23:29:42 +0200

    Update CLI for server

--- packages/server/src/cli.ts
+++ packages/server/src/cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { resolve } from "path";
+import { existsSync } from "fs";
 import yargs from "yargs";
 
 import { express, useEasyDB } from "./";
@@ -9,22 +10,30 @@ const options = yargs
     .option("p", { alias: "port", describe: "PORT", type: "number", demandOption: false })
     .option("h", { alias: "html", describe: "Index HTML file", type: "string", demandOption: false })
     .option("t", { alias: "easy-db-token", describe: "Security token", type: "string", demandOption: false })
+    .option("v", { alias: "verbose", describe: "0 - no, 1 - yes", type: "number", default: 1, demandOption: false })
+    .option("c", { alias: "cors", describe: "Use CORS", type: "boolean", default: true, demandOption: false })
+    .option("s", { alias: "size", describe: "Request size limit", type: "string", default: "15MB", demandOption: false })
     .argv;
 
-const PORT = process.env.PORT || options.port || 80;
-const INDEX = options.index || resolve(__dirname, "..", "index.html");
+const PORT = process.env.PORT || options.p || 80;
 const TOKEN = process.env.EASY_DB_TOKEN || options.t || null;
+const HTML = options.h || resolve(__dirname, "..", "index.html");
 
 const app = express();
 
-if (typeof INDEX === "string") {
+if (existsSync(HTML)) {
     app.get("/", function (req, res) {
-        res.sendFile(INDEX);
+        res.sendFile(HTML);
     });
+} else {
+    console.log(`There is not a file on "${HTML}".`);
+    process.exit();
 }
 
 useEasyDB(app, {
-    cors: true,
+    verbose: options.v,
+    requestSizeLimit: options.s,
+    cors: options.c,
     token: typeof TOKEN === "string" ? TOKEN : null,
 });
 

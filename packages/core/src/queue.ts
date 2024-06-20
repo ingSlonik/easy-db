df2c29a5596d3b63815ff55b41e1f637f4ec91e2
Filip Paulů <ing.fenix@seznam.cz>
2024-06-20 23:37:06 +0200

    Add reject to the queue

--- packages/core/src/queue.ts
+++ packages/core/src/queue.ts
@@ -2,10 +2,10 @@ export function addToQueue<T>(queue: null | Promise<any>, fn: () => Promise<T>):
     const promise = queue !== null ? queue : Promise.resolve();
 
     return new Promise((resolve, reject) => {
-        promise.then((r) => {
-            fn().then(result => {
-                resolve(result);
-            });
+        promise.then(() => {
+            fn()
+                .then(resolve)
+                .catch(reject);
         });
     });
 }

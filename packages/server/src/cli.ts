54eaf80bac9a7b86f27dd03942a168554d510d59
Filip Paulů <ing.fenix@seznam.cz>
2024-06-20 23:42:08 +0200

    Implement new yargs CLI for server

--- packages/server/src/cli.ts
+++ packages/server/src/cli.ts
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 import { resolve } from "path";
 import { existsSync } from "fs";
-import yargs from "yargs";
+import yargs from "yargs/yargs";
 
 import { express, useEasyDB } from "./";
 
-const options = yargs
+const options = yargs(process.argv.slice(2))
     .usage("Usage: easy-db-server --port <port> -html ./index.html")
-    .option("p", { alias: "port", describe: "PORT", type: "number", demandOption: false })
-    .option("h", { alias: "html", describe: "Index HTML file", type: "string", demandOption: false })
-    .option("t", { alias: "easy-db-token", describe: "Security token", type: "string", demandOption: false })
-    .option("v", { alias: "verbose", describe: "0 - no, 1 - yes", type: "number", default: 1, demandOption: false })
-    .option("c", { alias: "cors", describe: "Use CORS", type: "boolean", default: true, demandOption: false })
-    .option("s", { alias: "size", describe: "Request size limit", type: "string", default: "15MB", demandOption: false })
-    .argv;
+    .options({
+        p: { alias: "port", describe: "PORT", type: "number", demandOption: false },
+        h: { alias: "html", describe: "Index HTML file", type: "string", demandOption: false },
+        t: { alias: "easy-db-token", describe: "Security token", type: "string", demandOption: false },
+        v: { alias: "verbose", describe: "0 - no, 1 - yes", type: "number", default: 1, demandOption: false },
+        c: { alias: "cors", describe: "Use CORS", type: "boolean", default: true, demandOption: false },
+        s: { alias: "size", describe: "Request size limit", type: "string", default: "15MB", demandOption: false },
+    }).parseSync();
 
 const PORT = process.env.PORT || options.p || 80;
 const TOKEN = process.env.EASY_DB_TOKEN || options.t || null;

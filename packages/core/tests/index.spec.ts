d513e44c1649ed14e63bbedc155cec28b620eeb0
Filip Paulů <ing.fenix@seznam.cz>
2024-06-20 23:37:58 +0200

    Update tests for type based API

--- packages/core/tests/index.spec.ts
+++ packages/core/tests/index.spec.ts
@@ -1,8 +1,14 @@
-import chai, { assert, expect } from 'chai';
+import { use } from 'chai';
 import spies from 'chai-spies';
-chai.use(spies);
 
-import easyDBCore, { Data } from "../src/index";
+import easyDBCore, { File, Data } from "../src/index";
+
+type TestDB = {
+    test: { picture: File },
+    notUse: { error: string }
+}
+
+const { spy, assert, expect } = use(spies);
 
 const DUMMY_FILE = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=";
 
@@ -16,6 +22,7 @@ describe('Easy DB Core', () => {
         assert.equal(typeof easyDB, "object");
         assert.equal(typeof easyDB.insert, "function", "EasyDB doesn't set 'insert' method.");
         assert.equal(typeof easyDB.select, "function", "EasyDB doesn't set 'select' method.");
+        assert.equal(typeof easyDB.selectArray, "function", "EasyDB doesn't set 'selectArray' method.");
         assert.equal(typeof easyDB.update, "function", "EasyDB doesn't set 'update' method.");
         assert.equal(typeof easyDB.remove, "function", "EasyDB doesn't set 'remove' method.");
     });
@@ -30,15 +37,15 @@ describe('Easy DB Core', () => {
     });
 
     it('select all', async () => {
-        const testData = { "id": { "hello": "World" } };
-        const easyDB = easyDBCore({
+        const testData: any = { "id": { "hello": "World" } };
+        const easyDB = easyDBCore<TestDB>({
             async saveCollection(name: string, data: Data) { },
-            async loadCollection(name: string): Promise<null | Data> { 
+            async loadCollection(name: string): Promise<null | Data> {
                 if (name === "test") {
                     return testData;
                 } else {
-                    return null; 
-                }   
+                    return null;
+                }
             },
         });
         const data = await easyDB.select("test");
@@ -46,26 +53,44 @@ describe('Easy DB Core', () => {
     });
 
     it('select', async () => {
-        const testData = { "id": { "hello": "World" } };
-        const easyDB = easyDBCore({
+        const testData: any = { "id": { "hello": "World" } };
+        const easyDB = easyDBCore<TestDB>({
             async saveCollection(name: string, data: Data) { },
-            async loadCollection(name: string): Promise<null | Data> { 
+            async loadCollection(name: string): Promise<null | Data> {
                 if (name === "test") {
                     return testData;
                 } else {
-                    return null; 
-                }   
+                    return null;
+                }
             },
         });
 
         const data = await easyDB.select("test", "id");
-        assert.deepEqual(data, testData.id);
+        assert.deepEqual(data, { _id: "id", ...testData.id });
+    });
+
+    it('selectArray', async () => {
+        const testData: any = { "id": { "hello": "World" } };
+        const easyDB = easyDBCore<TestDB>({
+            async saveCollection(name: string, data: Data) { },
+            async loadCollection(name: string): Promise<null | Data> {
+                if (name === "test") {
+                    return testData;
+                } else {
+                    return null;
+                }
+            },
+        });
+
+        const data = await easyDB.selectArray("test");
+        assert.isArray(data);
+        assert.deepEqual(data[0]._id, "id");
     });
 
     it('update', async () => {
         const testData = { "id": { "hello": "World" } };
         const easyDB = easyDBCore({
-            async saveCollection(name: string, data: Data) { 
+            async saveCollection(name: string, data: Data) {
                 assert.equal(name, "test");
                 assert.deepEqual(data, testData);
             },
@@ -76,14 +101,14 @@ describe('Easy DB Core', () => {
 
     it('remove', async () => {
         const easyDB = easyDBCore({
-            async saveCollection(name: string, data: Data) { 
+            async saveCollection(name: string, data: Data) {
                 assert.equal(name, "test");
                 assert.deepEqual(data, {
                     "0": { "value": "Hello" },
                     "2": { "value": "What?" },
                 });
             },
-            async loadCollection(name: string): Promise<null | Data> { 
+            async loadCollection(name: string): Promise<null | Data> {
                 if (name === "test") {
                     return {
                         "0": { "value": "Hello" },
@@ -91,7 +116,7 @@ describe('Easy DB Core', () => {
                         "2": { "value": "What?" },
                     };
                 } else {
-                    return null; 
+                    return null;
                 }
             },
         });
@@ -99,61 +124,194 @@ describe('Easy DB Core', () => {
     });
 
     it('add file', async () => {
-        const easyDB = easyDBCore({
-            async saveCollection(name: string, data: Data) { },
-            async loadCollection(name: string): Promise<null | Data> { return null; },
+        const saveSpy = spy((base64: string) => { });
+
+        let testCollection = {};
+        let fileCollection = {};
+
+        const easyDB = easyDBCore<TestDB>({
+            async saveCollection(name: string, data: Data) {
+                if (name === "test") {
+                    testCollection = data;
+                } else if (name === "easy-db-files") {
+                    fileCollection = data;
+                }
+            },
+            async loadCollection(name: string): Promise<null | Data> {
+                if (name === "test") {
+                    return testCollection;
+                } else if (name === "easy-db-files") {
+                    return fileCollection;
+                } else {
+                    return null;
+                }
+            },
             async saveFile(base64: string) {
-                assert.equal(base64, DUMMY_FILE);
+                saveSpy(base64);
                 return "url/abc.png";
             },
             async removeFile(filePath: string) {
                 return;
             },
         });
-        await easyDB.insert("test", { picture: easyDB.file(DUMMY_FILE) });
+        const id = await easyDB.insert("test", { picture: easyDB.file(DUMMY_FILE) });
+        const row = await easyDB.select("test", id);
+
+        expect(saveSpy).to.have.been.called.with(DUMMY_FILE);
+        assert.equal(row?.picture.url, "url/abc.png");
     });
 
-    it('update file', async () => {
+    it('update row with remove old file', async () => {
+        const saveSpy = spy((base64: string) => { });
+        const removeSpy = spy((filePath: string) => { });
+
+        let testCollection: Data = {
+            "1": { picture: { id: "a", type: "EASY_DB_FILE", url: "/url/abc.png" } }
+        };
+        let fileCollection: Data = {
+            "a": {
+                "url": "/url/abc.png",
+                "use": [
+                    { "collection": "test", "rowId": "1" }
+                ]
+            }
+        };
         const easyDB = easyDBCore({
-            async saveCollection(name: string, data: Data) {},
+            async saveCollection(name: string, data: Data) {
+                if (name === "test") {
+                    testCollection = data;
+                } else if (name === "easy-db-files") {
+                    fileCollection = data;
+                }
+            },
             async loadCollection(name: string): Promise<null | Data> {
-                return {
-                    "1": { picture: { id: "a", type: "EASY_DB_FILE", url: "/url/abc.png" } }
-                };
+                if (name === "test") {
+                    return testCollection;
+                } else if (name === "easy-db-files") {
+                    return fileCollection;
+                } else {
+                    return null;
+                }
             },
             async saveFile(base64: string) {
+                saveSpy(base64);
                 return "url/abc2.png";
             },
             async removeFile(filePath: string) {
-                assert.equal(filePath, "/url/abc.png");
+                removeSpy(filePath);
                 return;
             },
         });
+
         await easyDB.update("test", "1", { picture: easyDB.file(DUMMY_FILE) });
+
+        expect(saveSpy).to.have.been.called.once;
+        expect(saveSpy).to.have.been.called.with(DUMMY_FILE);
+        expect(removeSpy).to.have.been.called.once;
+        expect(removeSpy).to.have.been.called.with("/url/abc.png");
     });
+
+    it('update row with not remove old file', async () => {
+        const saveSpy = spy((base64: string) => { });
+        const removeSpy = spy((filePath: string) => { });
+
+        let testCollection: Data = {
+            "1": { picture: { id: "a", type: "EASY_DB_FILE", url: "/url/abc.png" } },
+            "2": { picture: { id: "a", type: "EASY_DB_FILE", url: "/url/abc.png" } },
+        };
+        let fileCollection: Data = {
+            "a": {
+                "url": "/url/abc.png",
+                "use": [
+                    { "collection": "test", "rowId": "1" },
+                    { "collection": "test", "rowId": "2" },
+                ]
+            }
+        };
+        const easyDB = easyDBCore<TestDB>({
+            async saveCollection(name: string, data: Data) {
+                if (name === "test") {
+                    testCollection = data;
+                } else if (name === "easy-db-files") {
+                    fileCollection = data;
+                }
+            },
+            async loadCollection(name: string): Promise<null | Data> {
+                if (name === "test") {
+                    return testCollection;
+                } else if (name === "easy-db-files") {
+                    return fileCollection;
+                } else {
+                    return null;
+                }
+            },
+            async saveFile(base64: string) {
+                saveSpy(base64);
+                return "url/abc2.png";
+            },
+            async removeFile(filePath: string) {
+                removeSpy(filePath);
+                return;
+            },
+        });
+
+        await easyDB.update("test", "1", { picture: easyDB.file(DUMMY_FILE) });
+
+        expect(saveSpy).to.have.been.called.once;
+        expect(saveSpy).to.have.been.called.with(DUMMY_FILE);
+        expect(removeSpy).to.have.been.called.exactly(0);
+    });
+
 
     it('remove file', async () => {
+        const removeSpy = spy((filePath: string) => { });
+
+        let testCollection: Data = {
+            "1": { picture: { id: "a", type: "EASY_DB_FILE", url: "/url/abc.png" } },
+        };
+        let fileCollection: Data = {
+            "a": {
+                "url": "/url/abc.png",
+                "use": [
+                    { "collection": "test", "rowId": "1" },
+                ]
+            }
+        };
         const easyDB = easyDBCore({
-            async saveCollection(name: string, data: Data) {},
+            async saveCollection(name: string, data: Data) {
+                if (name === "test") {
+                    testCollection = data;
+                } else if (name === "easy-db-files") {
+                    fileCollection = data;
+                }
+            },
             async loadCollection(name: string): Promise<null | Data> {
-                return {
-                    "1": { picture: { id: "a", type: "EASY_DB_FILE", url: "/url/abc.png" } }
-                };
+                if (name === "test") {
+                    return testCollection;
+                } else if (name === "easy-db-files") {
+                    return fileCollection;
+                } else {
+                    return null;
+                }
             },
             async saveFile(base64: string) {
                 return "url/abc2.png";
             },
             async removeFile(filePath: string) {
-                assert.equal(filePath, "/url/abc.png");
+                removeSpy(filePath);
                 return;
             },
         });
+
         await easyDB.remove("test", "1");
+
+        expect(removeSpy).to.have.been.called.once;
+        expect(removeSpy).to.have.been.called.with("/url/abc.png");
     });
 
     it('rewrite file db only one with more changes', async () => {
-        const saveSpy = chai.spy(() => {});
-        const loadSpy = chai.spy(() => {});
+        const saveSpy = spy(() => { });
+        const loadSpy = spy(() => { });
 
         const savedFile = { id: "a", type: "EASY_DB_FILE", url: "/url/abc.png" };
         const easyDB = easyDBCore({
@@ -179,7 +337,7 @@ describe('Easy DB Core', () => {
             file2: easyDB.file(DUMMY_FILE),
             file3: easyDB.file(DUMMY_FILE),
         });
-        
+
         expect(saveSpy).to.have.been.called.once;
         expect(loadSpy).to.have.been.called.once;
     });
@@ -188,12 +346,12 @@ describe('Easy DB Core', () => {
         const row = {
             string: "String",
             number: 21,
-            array: [1,2,3],
+            array: [1, 2, 3],
             object: { a: "a" },
         };
 
-        const easyDB = easyDBCore({
-            async saveCollection(name: string, data: Data) {},
+        const easyDB = easyDBCore<{ test: typeof row }>({
+            async saveCollection(name: string, data: Data) { },
             async loadCollection(name: string): Promise<null | Data> {
                 return {
                     "1": row
@@ -206,8 +364,13 @@ describe('Easy DB Core', () => {
                 return;
             },
         });
-        const data = await easyDB.select<typeof row>("test", "1");
-        
+        const data = await easyDB.select("test", "1");
+
+        if (data === null) {
+            assert.fail("Selected row is null");
+            return;
+        }
+
         assert.isString(data.string);
         assert.isNumber(data.number);
         assert.isArray(data.array);

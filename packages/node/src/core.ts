db2f7f7f1708db77ddc4192f63d01a53e20bb44e
Filip Paulů <ing.fenix@seznam.cz>
2024-06-20 23:40:33 +0200

    Implement type based API for node

--- packages/node/src/core.ts
+++ packages/node/src/core.ts
@@ -2,9 +2,13 @@ import { parse } from "path";
 
 import JSZip from "jszip";
 import mimeDB from "mime-db";
-import easyDB, { getRandomId, Data } from "easy-db-core";
+import easyDB, { getRandomId, Data, API, DBTypes } from "easy-db-core";
 
 export type FileType = "collection" | "file" | "backup";
+
+export type NodeAPI<T extends DBTypes> = API<T> & {
+    getFileNames: (type: FileType) => Promise<string[]>,
+};
 
 export type Backup = {
     getActualName: () => string,
@@ -43,76 +47,89 @@ export const defaultBackupConfiguration: Backup = {
     },
 }
 
-export default function easyDBNodeCore({ cacheExpirationTime, backup, saveFiles, isFile, readFile, writeFile, unlinkFile, getFileNames }: Configuration) {
+export default function easyDBNodeCore<T extends DBTypes>({
+    cacheExpirationTime, backup, saveFiles, isFile, readFile, writeFile, unlinkFile, getFileNames
+}: Configuration): NodeAPI<T> {
 
-    return easyDB({
-        cacheExpirationTime,
-        async saveCollection(name: string, data: Data) {
-            // backup
-            if (backup !== false) {
-                const { keepName, getActualName } = backup === true ?
-                    defaultBackupConfiguration :
-                    { ...defaultBackupConfiguration, ...backup };
+    // not ask is file every save collection
+    let lastBackupActualName = "";
 
-                const backupName = `${getActualName()}.zip`;
-                if (!(await isFile("backup", backupName))) {
-                    const files = await getFileNames("collection");
-                    if (files.length > 0) {
-                        const zip = new JSZip();
+    return {
+        getFileNames,
+        ...easyDB({
+            cacheExpirationTime,
+            async saveCollection(name: string, data: Data) {
+                // backup
 
-                        files.forEach(file => zip.file(file, readFile("collection", file)));
-                        await writeFile("backup", backupName, await zip.generateAsync({ type: "nodebuffer" }));
-                    }
+                if (backup !== false) {
+                    const { keepName, getActualName } = backup === true ?
+                        defaultBackupConfiguration :
+                        { ...defaultBackupConfiguration, ...backup };
 
-                    const backupFiles = await getFileNames("backup");
-                    for (const backupFile of backupFiles) {
-                        const { name, ext } = parse(backupFile);
-                        if (ext === ".zip" && !keepName(name)) {
-                            await unlinkFile("backup", backupFile);
+                    const actualName = getActualName();
+                    if (actualName !== lastBackupActualName) {
+                        lastBackupActualName = actualName;
+
+                        const backupName = `${actualName}.zip`;
+                        if (!(await isFile("backup", backupName))) {
+                            const files = await getFileNames("collection");
+                            if (files.length > 0) {
+                                const zip = new JSZip();
+                                files.forEach(file => zip.file(file, readFile("collection", file)));
+                                await writeFile("backup", backupName, await zip.generateAsync({ type: "nodebuffer" }));
+                            }
+
+                            const backupFiles = await getFileNames("backup");
+                            for (const backupFile of backupFiles) {
+                                const { name, ext } = parse(backupFile);
+                                if (ext === ".zip" && !keepName(name)) {
+                                    await unlinkFile("backup", backupFile);
+                                }
+                            }
                         }
                     }
                 }
-            }
 
-            // save collection
-            await writeFile("collection", `${name}.json`, Buffer.from(JSON.stringify(data, null, "\t"), "utf8"));
-        },
-        async loadCollection(name: string): Promise<null | Data> {
-            const file = `${name}.json`;
-            if (await isFile("collection", file)) {
-                const content = await readFile("collection", file);
-                try {
-                    const data = JSON.parse(content.toString());
-                    if (data !== null && typeof data === "object") {
-                        return data;
-                    } else {
+                // save collection
+                await writeFile("collection", `${name}.json`, Buffer.from(JSON.stringify(data, null, "\t"), "utf8"));
+            },
+            async loadCollection(name: string): Promise<null | Data> {
+                const file = `${name}.json`;
+                if (await isFile("collection", file)) {
+                    const content = await readFile("collection", file);
+                    try {
+                        const data = JSON.parse(content.toString());
+                        if (data !== null && typeof data === "object") {
+                            return data;
+                        } else {
+                            return null;
+                        }
+                    } catch (e) {
+                        // save inconsistent data
+                        const wrongFileName = `${name}-wrong-${getDateFileName()}.json`;
+                        // rename doesn't work when is problem
+                        await writeFile("collection", wrongFileName, content);
+                        // await copyFile(file, resolve(configuration.folderDB, wrongFileName));
+                        console.error(`Collection "${name}" is not parsable. It is save to "${wrongFileName}".`);
                         return null;
                     }
-                } catch (e) {
-                    // save inconsistent data
-                    const wrongFileName = `${name}-wrong-${getDateFileName()}.json`;
-                    // rename doesn't work when is problem
-                    await writeFile("collection", wrongFileName, content);
-                    // await copyFile(file, resolve(configuration.folderDB, wrongFileName));
-                    console.error(`Collection "${name}" is not parsable. It is save to "${wrongFileName}".`);
+                } else {
                     return null;
                 }
-            } else {
-                return null;
-            }
-        },
-        saveFile: !saveFiles ? undefined : async (base64: string) => {
-            const extension = getFileExtension(base64);
-            const name = await getFreeName("file", extension, isFile);
+            },
+            saveFile: !saveFiles ? undefined : async (base64: string) => {
+                const extension = getFileExtension(base64);
+                const name = await getFreeName("file", extension, isFile);
 
-            return await writeFile("file", name, Buffer.from(getClearBase64(base64), "base64"));
-        },
-        removeFile: !saveFiles ? undefined : async (path: string) => {
-            if (await isFile("file", path)) {
-                await unlinkFile("file", path);
+                return await writeFile("file", name, Buffer.from(getClearBase64(base64), "base64"));
+            },
+            removeFile: !saveFiles ? undefined : async (path: string) => {
+                if (await isFile("file", path)) {
+                    await unlinkFile("file", path);
+                }
             }
-        }
-    });
+        }),
+    };
 }
 
 async function getFreeName(type: FileType, extension: string, isFile: (type: FileType, name: string) => Promise<boolean>): Promise<string> {

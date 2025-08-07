4ac2c08e1b30927428e41d2fe76de00108dabc01
Filip Paulů <ing.fenix@seznam.cz>
2025-08-07 08:49:43 +0200

    Fix promise finally in queue

--- packages/core/src/queue.ts
+++ packages/core/src/queue.ts
@@ -2,7 +2,7 @@ export function addToQueue<T>(queue: null | Promise<any>, fn: () => Promise<T>):
     const promise = queue !== null ? queue : Promise.resolve();
 
     return new Promise((resolve, reject) => {
-        promise.then(() => {
+        promise.finally(() => {
             fn()
                 .then(resolve)
                 .catch(reject);
